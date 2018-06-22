39b9c5ef51234c1bda020391a1074e9488932391
Robert Matych <dev@regattatropicals.com>
2018-06-22 09:20:45 -0700

    Adjust WebPack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,64 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 
-const htmlPlugin = new HtmlWebPackPlugin({
-  template: './static/app.html',
-  filename: './app.html'
+const minificationOptions = {
+    removeAttributeQuotes: true,
+    collapseWhitespace: true,
+    minifyCSS: true
+};
+
+const appHtmlPlugin = new HtmlWebPackPlugin({
+    template: './static/app.html',
+    filename: './app.html',
+    minify: minificationOptions
+});
+
+const loginHtmlPlugin = new HtmlWebPackPlugin({
+    template: './static/login.html',
+    filename: './login.html',
+    inject: false,
+    minify: minificationOptions
 });
 
 module.exports = {
-  module: {
-    rules: [
-        {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-                loader: 'babel-loader'
-            }
-        },
-        {
-            test: /\.css$/,
-            use: [
-                {
-                    loader: "style-loader"
-                },
-                {
-                    loader: "css-loader",
-                    options: {
-                        modules: true,
-                        importLoaders: 1,
-                        localIdentName: "[name]_[local]_[hash:base64]",
-                        sourceMap: true,
-                        minimize: true
-                    }
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
                 }
-            ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: "style-loader"
+                    },
+                    {
+                        loader: "css-loader",
+                        options: {
+                            modules: true,
+                            importLoaders: 1,
+                            localIdentName: "[name]_[local]_[hash:base64]",
+                            sourceMap: true,
+                            minimize: true
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+
+    plugins: [ appHtmlPlugin, loginHtmlPlugin ],
+
+    devServer: {
+        contentBase: 'dist',
+        watchContentBase: true,
+        index: 'login.html',
+        proxy: {
+            '/login': 'http://localhost:33456',
+            '/api': 'http://localhost:33456'
         }
-    ]
-  },
-  plugins: [ htmlPlugin ]
+    }
 };
